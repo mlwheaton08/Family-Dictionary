b04153a5a67b29f0b9c7544cde4ac10f3aa37952
Mason Wheaton <mlwheaton06@gmail.com>
2023-01-23 21:30:47 -0600

    Add project files.

---
+++ Family Dictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+myFamily.Add("sister", new Dictionary<string, string>() { { "name", "Krista" }, { "age", "20" } });
+myFamily.Add("brother", new Dictionary<string, string>() { { "name", "Jack" }, { "age", "22" } });
+myFamily.Add("mother", new Dictionary<string, string>() { { "name", "Lora" }, { "age", "56" } });
+
+foreach (var member in myFamily)
+{
+    string relation = member.Key;
+    string name = member.Value["name"];
+    string age = member.Value["age"];
+    Console.WriteLine($"{name} is my {relation} and is {age} years old.");
+}
